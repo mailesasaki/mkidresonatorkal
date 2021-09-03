901ead9dafbd9320220a57cfc88af5f014e87f31
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-09-02 22:43:18 -0700

    Update wpsnn_yaml_try.py

--- mkidresonatorkal/wpsnn_yaml_try.py
+++ mkidresonatorkal/wpsnn_yaml_try.py
@@ -1,3 +1,7 @@
+"""
+Trains the model when given an mlDict file
+"""
+
 import wpsnnmkidkal2
 import mkidcore.config as config
 import argparse
@@ -5,12 +9,14 @@ import os
 
 parser = argparse.ArgumentParser(description='ML model trainer')
 parser.add_argument('mlDict', help='yaml file containing mlDict information')
+parser.add_argument('model_name', help='destination of ML model')
 args=parser.parse_args()
 
 if os.path.isfile(args.mlDict) == False:
         raise Exception('mlDict file does not exist')
     
 mlDict_file = args.mlDict
+model_name = args.model_name
 mlDict = config.load(mlDict_file)
 
 trial1 = wpsnnmkidkal2.WPSNeuralNet(mlDict)
